8ec62060a4cbfda5f5a82ec80b057c398d8f10dd
orlymariela <omss1336@gmail.com>
2023-08-15 21:36:50 -0300

    Button

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -9,13 +9,17 @@ const Home = () => {
         <p className='text-gray-300 '>Hi, my name is</p>
         <h1 className='text-4xl  sm:text-7xl text-white'>Orly Santiago</h1>
         <h2 className='text-2xl sm:text-7xl text-[#8892b0]'>Front-end developer.</h2>
-        <p className='text-gray-300 py-4 max-w-100px'>A passionate front-end developer with a deep love for creating amazing and functional web experiences, combining visual aesthetics with seamless functionality to deliver digital products that captivate users.</p>
-        <div>
-          <button className='relative flex items-center justify-center overflow-hidden trasition duration-300 ease-out group border-2 px-6 py-4 my-2 text-gray-300 '>
-            <HiArrowNarrowRight className='absolute flex items-center w-full h-full text-white duration-300 -translate-x-full bg-[#27272a] group-hover:translate-x-0 ease' />
-            <span className='absolute flex items-center justify-center w-full h-full text-blue-200 transition-all duration-300 transform group-hover:translate-x-0 ease'>View more
-            </span> </button>
-        </div>
+        <p className='text-gray-300 py-4 max-w-100px'> A passionate front-end developer with a deep love for creating amazing and functional web experiences, combining visual aesthetics with seamless functionality to deliver digital products that captivate users.</p>
+          <button className='text-lg font-medium text-gray-300 px-10 p-3 my-2 hover:text-[white] group relative flex items-center overflow-hidden'>
+            <span className='absolute left-0 w-full h-0 transition-all bg-[#27272a] opacity-100 group-hover:h-full group-hover:top-0 duration-200 ease'>
+            </span>
+            <span className='absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease'>
+              <HiArrowNarrowRight className='ml-2'/>
+            </span>
+            <span className='relative'>
+              View More
+            </span>
+          </button>
       </div>
     </div>
 
